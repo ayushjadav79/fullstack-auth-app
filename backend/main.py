436a55b9ee23305fa435722967b1ba02e1824e7f
Ayush Jadav <ayushjadav79@gmail.com>
2026-02-12 11:45:08 +0530

    test commit 2 for running

--- backend/main.py
+++ backend/main.py
@@ -28,5 +28,5 @@ app.include_router(router)
 
 @app.get("/")
 def read_root():
-    print("Change from branch feature_1 with ECR ")
+    print("Change from branch feature_1 with ECR")
     return {"message": "Backend is running and connected to S3"}
