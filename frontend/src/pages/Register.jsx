760dda0d98ce9a671bcece77883a4d3cac14425e
Ayush Jadav <ayushjadav79@gmail.com>
2026-02-12 20:39:51 +0530

    Made Frontend Docker

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState , useEffect } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import.meta.env.VITE_API_URL
 
 const Register = () => {
     const [availableHobbies, setAvailableHobbies] = useState([]);
--- frontend/src/pages/UserList.jsx
+++ frontend/src/pages/UserList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import.meta.env.VITE_API_URL
 
 const UserList = () => {
     const [users, setUsers] = useState([]);
